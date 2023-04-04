b0b006f7f1a166a5cc6ee828033e8eaec396c6a8
nauticalstars <nauticalstars21@gmail.com>
2023-04-03 20:34:24 -0700

    installed yarn

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Paris</h1>
+      <ul>
+        <li>Monday 8:00 p.m</li>
+        <li>Mostly cloudy</li>
+      </ul>
+    </div>
+  );
+}
